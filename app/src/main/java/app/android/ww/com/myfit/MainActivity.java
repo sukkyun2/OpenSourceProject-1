e0886d5336c002081085b888b515f88706b61a8e
noteasy-code <65408372+noteasy-code@users.noreply.github.com>
2020-06-04 21:54:25 +0900

    init

---
+++ app/src/main/java/app/android/ww/com/myfit/MainActivity.java
@@ -0,0 +1,10 @@
+package app.android.ww.com.myfit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
