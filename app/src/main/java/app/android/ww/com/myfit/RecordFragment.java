7e00aa5fcddf0fced22f571cc4e9628acd763c42
HongSukKyun <goobghd@gmail.com>
2020-06-14 19:05:30 +0900

    6월 2주차 버전

--- app/src/main/java/app/android/ww/com/myfit/RecordFragment.java
+++ app/src/main/java/app/android/ww/com/myfit/RecordFragment.java
@@ -17,10 +17,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.Registry;
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
